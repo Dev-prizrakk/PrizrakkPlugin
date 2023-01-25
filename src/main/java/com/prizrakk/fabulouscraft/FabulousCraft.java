e72eed267b0e45c639a17be549f20c88bea69b7d
Alexander (prizrakk) <88229427+Dev-prizrakk@users.noreply.github.com>
2023-01-25 18:50:30 +0300

    Почистил main класс от коментов и тд

--- src/main/java/com/prizrakk/fabulouscraft/FabulousCraft.java
+++ src/main/java/com/prizrakk/fabulouscraft/FabulousCraft.java
@@ -3,12 +3,9 @@ package com.prizrakk.fabulouscraft;
 import com.prizrakk.fabulouscraft.commands.*;
 import com.prizrakk.fabulouscraft.db.Database;
 import com.prizrakk.fabulouscraft.handler.ServerPing;
-import com.sun.net.httpserver.HttpServer;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.net.InetSocketAddress;
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
@@ -23,19 +20,14 @@ public final class FabulousCraft extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-
-        // Создаем конфиг
         saveDefaultConfig();
-        // Тестовая шняга
-
+        log.info(ChatColor.GOLD + "Плагин запущен!");
+        log.info(ChatColor.GOLD + "Версия: Pre-Release 1.0");
         try {
             ServerPing.main((String[])null);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        // Лог о запуске
-        log.info(ChatColor.GOLD + "Версия: "+ getConfig().getString("config.version"));
-        //Подключаем к базе данных!
         try {
             this.database = new Database(this);
             database.initializeDatabase();
@@ -43,22 +35,21 @@ public final class FabulousCraft extends JavaPlugin {
             log.warning(ChatColor.RED + "Ошибка Базы Данных!");
             ex.printStackTrace();
         }
-        //getServer().getPluginManager().registerEvents(new Listeners(this), this);
-        // Грузим эвенты!
+
         getServer().getPluginManager().registerEvents(new ServerPing(this),this);
-        //getServer().getPluginManager().registerEvents(new SimpleEventHandler(this), this);
-        // Подгружаем команды
+
         getServer().getPluginCommand("heal").setExecutor(new HealCommand(this));
         getServer().getPluginCommand("feed").setExecutor(new FeedCommand(this));
         getServer().getPluginCommand("gm").setExecutor(new gmSurvivalCommand(this));
         getServer().getPluginCommand("day").setExecutor(new DayCommand(this));
         getServer().getPluginCommand("prizrakk").setExecutor(new SystemCommand(this));
-        //getServer().getPluginCommand("adm_warn").setExecutor(new AdminWarnCommand(this));
     }
 
 
     @Override
-    public void onDisable() {}
+    public void onDisable() {
+        log.info(ChatColor.RED + "Плагин отключен!");
+    }
 
     public Database getDatabase() {
         return database;
